cd8d9e07e27ab9ef3255283208f5beb498ade1f5
Emanuel <emanuel-caue@hotmail.com>
2023-11-06 14:43:54 -0300

    feat: prisma sqlite

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -2,13 +2,30 @@ import SchemaBuilder from '@pothos/core';
 
 const builder = new SchemaBuilder({});
 
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+
 builder.queryType({
   fields: (t) => ({
-    rota: t.string({
+    createUser: t.string({
       args: {
         name: t.arg.string(),
       },
-      resolve: (parent, { name }) => `hello, ${name || 'World'}`,
+      resolve: async (parent, { name }) => {
+        // create a new user
+        await prisma.user.create({
+          data: {
+            name: "John Dough",
+            email: `john-${Math.random()}@example.com`,
+          },
+        });
+
+        // count the number of users
+        const count = await prisma.user.count();
+        return `There are ${count} users in the database.`
+      },
     }),
   }),
 });
